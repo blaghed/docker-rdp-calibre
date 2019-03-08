fdadad4cfe5b5dafb63d157d25b893aba8b6ba3c
blaghed <luis_cabral@yahoo.com>
2019-03-09 00:58:16 +0100

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -6,5 +6,9 @@ chown -R nobody:users /config
 chmod -R g+rw /config
 [[ -f /tmp/.X1-lock ]] && rm /tmp/.X1-lock && echo "X1-lock found, deleting"
 
-echo "Updating to latest version"
-wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+if [ ! "$EDGE" = "1" ]; then
+  echo "EDGE not requested, keeping stable version"
+else
+  echo "EDGE requested, updating to latest version"
+  wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+fi
